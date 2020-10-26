8671eded9d527fa10d24925fa29195fbba9cf177
lee <lee@underneath.ca>
2020-10-26 11:49:37 -0700

    use node-localstorage for tests

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,8 +1,13 @@
+import { LocalStorage } from "node-localstorage";
 const keyPrefix = "undo-redo";
 const currentSuffix = "-state-current";
 const currentStateKey = keyPrefix + currentSuffix;
 
+// for tests on nodejs
+if (typeof localStorage === "undefined") {
+  global.localStorage = new LocalStorage("./scratch");
+}
+
 export function saveCurrentState(state: {}): void {
   localStorage.setItem(currentStateKey, JSON.stringify(state));
 }
-
