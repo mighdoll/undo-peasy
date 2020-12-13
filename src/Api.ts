e30390ea26c589a49225eae2139c435ee744dce1
lee <lee@underneath.ca>
2020-12-12 23:51:53 -0800

    update api types

--- src/Api.ts
+++ src/Api.ts
@@ -1,6 +1,11 @@
-import { undoRedo, ActionFilter } from "./Middleware";
-import { KeyPathFilter, undoable, WithUndo } from "./Actions";
+import { undoRedo } from "./Middleware";
+import {
+  KeyPathFilter,
+  undoable,
+  WithUndo,
+  ActionStateFilter,
+} from "./Actions";
 
 export { undoRedo, undoable };
 
-export type { WithUndo, ActionFilter, KeyPathFilter };
+export type { WithUndo, ActionStateFilter, KeyPathFilter };
